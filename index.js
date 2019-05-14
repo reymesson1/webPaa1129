01b8b93742d13ecc6f276977468d1559577024de
messore <rey.messon@verizon.com>
2019-05-14 15:31:58 -0400

    Invoice

---
+++ index.js
@@ -0,0 +1,62 @@
+var ejs = require('./utils/ejs');
+var pdf = require('./utils/pdf');
+var moment = require('moment');
+var mongoose = require('mongoose');
+var data = require('./data/data.json');
+var express = require('express'),
+fs = require('fs'),
+app = express();
+var Master = require('./models/master.js');
+
+app.get('/:id', async (req, res) =>{
+
+    var fileName = moment().format('YYYYMMDDHHmmSS')+'.pdf'
+
+    var data = await Master.find({"id":req.params.id})
+
+    console.log(data)
+    //console.log(data2[0].item)
+
+    ejs.toHTML('./templates/index.ejs', data[0]).then(function (html) {
+        var options = { format: 'Letter' };
+        // var output = './data/out/pdf_' + moment().format('YYYYMMDDHHmmSS') + '.pdf'
+        var output = './data/out/pdf_' + fileName
+    
+        pdf.toPDF(html, options, output).then(function (response) {
+            console.log("PDF file successfully written");
+            console.log(response);
+        }, function (error) {
+            console.error(error);
+        });
+    }, function (error) {
+       console.error(error);
+    });
+    
+
+    setTimeout(() => {
+    
+        // var filePath = "/files/my_pdf_file.pdf";
+        var filePath = "/data/out/pdf_"+fileName;
+        
+        fs.readFile(__dirname + filePath , function (err,data){
+            res.contentType("application/pdf");
+            res.send(data);
+        });
+
+    }, 5000);
+
+    
+
+
+});
+
+
+mongoose.connect('mongodb://localhost:27017/supreme',(err)=>{
+    if(!err){
+        console.log('Connected to mongo Database');
+    }
+})
+
+app.listen(3000, function(){
+console.log('Listening on 3000');
+});
---
+++ models/master.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = new mongoose.Schema({
+    id: String,
+    idOrder: String,
+    date: String,
+    name: String,
+    project: Number,
+    agregado: Number,
+    desc: Number,
+    itbis: Number,
+    grandTotal: Number,
+    fechaentrega: String,
+    horaentrega: String,
+    balance: Number,
+    pending: Number,
+    current: Number,
+    tipopago: String,
+    ncf: String,
+    status: String,
+    item: Array,
+    comments: Array,
+    payment: String
+})
+
+module.exports = mongoose.model('Master', userSchema)
